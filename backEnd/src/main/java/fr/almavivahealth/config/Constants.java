9afc84230f12465b408cdc47ab4c45c86ed89a59
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-03-15 21:13:45 +0100

    add class of Constants

---
+++ backEnd/src/main/java/fr/almavivahealth/config/Constants.java
@@ -0,0 +1,14 @@
+package fr.almavivahealth.config;
+
+/**
+ * Application constants for managing constants.
+ * 
+ * @author christopher
+ *
+ */
+public final class Constants {
+
+	private Constants() {
+		// private constructor needed for Constants class.
+	}
+}
