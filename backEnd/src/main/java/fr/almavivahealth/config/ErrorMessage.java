3a733e962688c7e42fc30df1881e1fb2a701a168
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-03-15 21:13:59 +0100

    add class of ErrorMessage

---
+++ backEnd/src/main/java/fr/almavivahealth/config/ErrorMessage.java
@@ -0,0 +1,11 @@
+package fr.almavivahealth.config;
+
+/**
+ * Application constants for managing Errors Message.
+ * @author christopher
+ *
+ */
+public final class ErrorMessage {
+
+    private ErrorMessage() {}
+}
