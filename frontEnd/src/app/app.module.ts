f5ffd7aa0dcd98092492906ff81cb3ae1a3eff3b
NealKemissi <neal.k@hotmail.fr>
2020-03-24 16:43:04 +0100

    adding bootstrap + navbar

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
