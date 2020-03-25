ca7d7e6521cff8574eeeb6cc38f859b17f226c0a
NealKemissi <neal.k@hotmail.fr>
2020-03-26 00:00:53 +0100

    add patients list

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -8,10 +8,18 @@ import { AppComponent } from "./app.component";
 import { LoaderComponent } from "./components/loader/loader.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { MenuComponent } from "./components/navbar/menu/menu.component";
-import { PatientsComponent } from './components/patients/patients.component';
+import { PatientsComponent } from "./components/patients/patients.component";
+import { ListPatientsComponent } from './components/patients/list-patients/list-patients.component';
 
 @NgModule({
-  declarations: [AppComponent, LoaderComponent, NavbarComponent, MenuComponent, PatientsComponent],
+  declarations: [
+    AppComponent,
+    LoaderComponent,
+    NavbarComponent,
+    MenuComponent,
+    PatientsComponent,
+    ListPatientsComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
--- frontEnd/src/app/components/patients/patients.component.ts
+++ frontEnd/src/app/components/patients/patients.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-patients',
-  templateUrl: './patients.component.html',
-  styleUrls: ['./patients.component.scss']
+  selector: "app-patients",
+  templateUrl: "./patients.component.html",
+  styleUrls: ["./patients.component.scss"]
 })
 export class PatientsComponent implements OnInit {
+  addLogo = faUserPlus;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
