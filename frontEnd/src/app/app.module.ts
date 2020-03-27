df112d52fefe30eddc9fd12128efe1bec448e9d9
NealKemissi <neal.k@hotmail.fr>
2020-03-27 22:52:11 +0100

    add patient

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 import { AppComponent } from "./app.component";
@@ -9,14 +10,14 @@ import { LoaderComponent } from "./components/loader/loader.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { MenuComponent } from "./components/navbar/menu/menu.component";
 import { PatientsComponent } from "./components/patients/patients.component";
-import { ListPatientsComponent } from './components/patients/list-patients/list-patients.component';
-import { DetailPatientComponent } from './components/patients/detail-patient/detail-patient.component';
-import { PatientComponent } from './components/patient/patient.component';
-import { InfosPatientComponent } from './components/patient/infos-patient/infos-patient.component';
-import { FoodPatientComponent } from './components/patient/food-patient/food-patient.component';
-import { HistoryPatientComponent } from './components/patient/history-patient/history-patient.component';
-import { PatientHistoryComponent } from './components/patient-history/patient-history.component';
-import { PatientAddComponent } from './components/patient-add/patient-add.component';
+import { ListPatientsComponent } from "./components/patients/list-patients/list-patients.component";
+import { DetailPatientComponent } from "./components/patients/detail-patient/detail-patient.component";
+import { PatientComponent } from "./components/patient/patient.component";
+import { InfosPatientComponent } from "./components/patient/infos-patient/infos-patient.component";
+import { FoodPatientComponent } from "./components/patient/food-patient/food-patient.component";
+import { HistoryPatientComponent } from "./components/patient/history-patient/history-patient.component";
+import { PatientHistoryComponent } from "./components/patient-history/patient-history.component";
+import { PatientAddComponent } from "./components/patient-add/patient-add.component";
 
 @NgModule({
   declarations: [
@@ -34,7 +35,7 @@ import { PatientAddComponent } from './components/patient-add/patient-add.compon
     PatientHistoryComponent,
     PatientAddComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontEnd/src/app/components/patient-add/patient-add.component.ts
+++ frontEnd/src/app/components/patient-add/patient-add.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  faAngleDoubleLeft,
+  faPlus,
+  faMinus
+} from "@fortawesome/free-solid-svg-icons";
+import { Diet } from "src/app/models/diet";
+import { Texture } from "src/app/models/texture";
 
 @Component({
-  selector: 'app-patient-add',
-  templateUrl: './patient-add.component.html',
-  styleUrls: ['./patient-add.component.scss']
+  selector: "app-patient-add",
+  templateUrl: "./patient-add.component.html",
+  styleUrls: ["./patient-add.component.scss"]
 })
 export class PatientAddComponent implements OnInit {
+  goBackLogo = faAngleDoubleLeft;
+  addLogo = faPlus;
+  removeLogo = faMinus;
 
-  constructor() { }
+  allergy: string;
+  allergies: string[] = [];
 
-  ngOnInit(): void {
+  dietsAvailable: Array<Diet> = [
+    { id: 1, name: "Normale" },
+    { id: 2, name: "Sans porc" },
+    { id: 3, name: "Hyperprotéiné" },
+    { id: 4, name: "Sans sel" },
+    { id: 5, name: "Sans résidu" },
+    { id: 6, name: "Diabétique" }
+  ];
+  texturesAvailable: Array<Texture> = [
+    { id: 1, name: "Normale" },
+    { id: 2, name: "Haché" },
+    { id: 3, name: "Mixé" }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addAllergy(allergy: string): void {
+    if (allergy != undefined) {
+      this.allergies.push(allergy);
+    }
   }
 
+  deleteAllergy(allergy: string): void {
+    let i = this.allergies.indexOf(allergy);
+    this.allergies.splice(i, 1);
+  }
 }
