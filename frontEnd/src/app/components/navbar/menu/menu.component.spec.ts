8c6163f1a2ad60b258833c71782cde20c9adcfe6
NealKemissi <neal.k@hotmail.fr>
2020-04-07 23:43:24 +0200

    add 6 tests + fix 4 tests

--- frontEnd/src/app/components/navbar/menu/menu.component.spec.ts
+++ frontEnd/src/app/components/navbar/menu/menu.component.spec.ts
@@ -1,16 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { MenuComponent } from './menu.component';
+import { MenuComponent } from "./menu.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-describe('MenuComponent', () => {
+describe("MenuComponent", () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [MenuComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +21,7 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- frontEnd/src/app/services/login/login.service.spec.ts
+++ frontEnd/src/app/services/login/login.service.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { LoginService } from './login.service';
+import { LoginService } from "./login.service";
+import { LoginDTO } from "src/app/models/dto/loginDTO";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-describe('LoginService', () => {
+describe("LoginService", () => {
   let service: LoginService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(LoginService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("#login should return token of user", (done: DoneFn) => {
+    let token = "mLOZhh53QHNkxM_hwE27^rW@NIt{I6";
+    let loginDTO = new LoginDTO("username", "password");
+    service.login(loginDTO).subscribe((value) => {
+      expect(value).toBe(token);
+      done();
+    });
+  });
+
+  it("#login should return token of user", (done: DoneFn) => {
+    const loginServiceSpy = jasmine.createSpyObj("LoginService", ["login"]);
+    const expected = "mLOZhh53QHNkxM_hwE27^rW@NIt{I6";
+    loginServiceSpy.login.and.return(expected);
+    let loginDTO = new LoginDTO("username", "password");
+    service.login(loginDTO).subscribe((value) => {
+      expect(value).toBe(expected);
+      done();
+    });
   });
 });
