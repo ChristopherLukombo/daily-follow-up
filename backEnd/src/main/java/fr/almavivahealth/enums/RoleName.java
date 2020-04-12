8370bf824e5a0f5986e93365ff70ec7d18faa1d8
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-04-12 15:09:43 +0200

    add roles in enum RoleName

--- backEnd/src/main/java/fr/almavivahealth/enums/RoleName.java
+++ backEnd/src/main/java/fr/almavivahealth/enums/RoleName.java
@@ -1,6 +1,20 @@
 package fr.almavivahealth.enums;
 
+/**
+ * The Enum RoleName.
+ */
 public enum RoleName {
 
-	ROLE_ADMIN
+	/** The role patient. */
+	ROLE_PATIENT,
+
+	/** The role admin. */
+	ROLE_ADMIN,
+
+	/** The role diet. */
+	ROLE_DIET,
+
+	/** The role caregiver. */
+	ROLE_CAREGIVER;
+
 }
