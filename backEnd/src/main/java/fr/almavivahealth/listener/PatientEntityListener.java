d4c9590392b98b823a695a33c376fbdd711e792d
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-04-29 00:58:01 +0200

    feat (patient history): add status recreated in patient history

--- backEnd/src/main/java/fr/almavivahealth/listener/PatientEntityListener.java
+++ backEnd/src/main/java/fr/almavivahealth/listener/PatientEntityListener.java
@@ -1,5 +1,7 @@
 package fr.almavivahealth.listener;
 
+import java.util.Optional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
@@ -21,8 +23,13 @@ public class PatientEntityListener {
 
 	@PreUpdate
 	public void preUpdate(final Patient target) {
-		if (target.getState() != null && target.getState()) {
+		final boolean stateOfPatient = findStateOfPatient(target);
+		final boolean stateOfPreviousPatient = findStateOfPreviousPatient(target);
+		
+		if (stateOfPreviousPatient && stateOfPatient) {
 			perform(target, Action.UPDATED);
+		} else if (!stateOfPreviousPatient && stateOfPatient) {
+			perform(target, Action.RECREATED);
 		} else {
 			perform(target, Action.DELETED);
 		}
@@ -31,6 +38,19 @@ public class PatientEntityListener {
 	private void perform(final Patient target, final Action action) {
 		final EntityManager entityManager = BeanUtil.getBean(EntityManager.class);
 		entityManager.persist(new PatientHistory(target, action));
+	}
+	
+	private boolean findStateOfPatient(final Patient patient) {
+		return Optional.ofNullable(patient)
+				.map(Patient::getState)
+				.orElseGet(() -> false);
+	}
+	
+	private boolean findStateOfPreviousPatient(final Patient patient) {
+		return Optional.ofNullable(patient)
+				.map(Patient::getPreviousPatient)
+				.map(Patient::getState)
+				.orElseGet(() -> false);
 	}
 
 }
