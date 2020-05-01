0136d28b31739134048a4f0452e1bf97204b43f1
NealKemissi <neal.k@hotmail.fr>
2020-05-01 15:32:32 +0200

    pass data to child of child component + fix tests

--- frontEnd/src/app/components/patient/food-patient/comment-patient/comment-patient.component.spec.ts
+++ frontEnd/src/app/components/patient/food-patient/comment-patient/comment-patient.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CommentPatientComponent } from "./comment-patient.component";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule, ToastrService } from "ngx-toastr";
 
 describe("CommentPatientComponent", () => {
   let component: CommentPatientComponent;
@@ -9,8 +11,13 @@ describe("CommentPatientComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+      ],
       declarations: [CommentPatientComponent],
+      providers: [ToastrService],
     }).compileComponents();
   }));
 
