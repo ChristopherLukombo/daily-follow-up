b7f97b43e096b16fe42d8887bc284e7dfb5efd97
Dede <angelo.deliessche@gmail.com>
2020-05-07 19:05:48 +0200

    Up

--- frontEnd/src/environments/environment.prod.ts
+++ frontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   appRootUrl: "http://delissch.freeboxos.fr/api",
-  version: 4
+  version: 5
 };
--- frontEnd/src/environments/environment.ts
+++ frontEnd/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appRootUrl: "http://localhost:8081/api",
-  version: 4,
+  version: 5,
 };
 
 /*
