4b950dcf10c22a3b7262e6208d3bfcfe7e31bc31
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-05-10 23:25:19 +0200

    Merge branch 'develop'

--- backEnd/src/main/java/fr/almavivahealth/config/WebMvcConfig.java
+++ backEnd/src/main/java/fr/almavivahealth/config/WebMvcConfig.java
@@ -1,19 +1,13 @@
 package fr.almavivahealth.config;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import fr.almavivahealth.config.interceptor.PatientImportationInterceptor;
 
@@ -22,52 +16,27 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final PatientImportationInterceptor patientImportationInterceptor;
 
-    @Autowired
+	@Autowired
 	public WebMvcConfig(final PatientImportationInterceptor patientImportationInterceptor) {
 		this.patientImportationInterceptor = patientImportationInterceptor;
 	}
 
 	@Bean
-    public LocaleResolver localeResolver() {
-        return new CookieLocaleResolver();
-    }
+	public LocaleResolver localeResolver() {
+		return new CookieLocaleResolver();
+	}
 
-    @Bean
-    public LocaleChangeInterceptor localeInterceptor() {
-        final LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
-        localeInterceptor.setParamName("lang");
-        return localeInterceptor;
-    }
+	@Bean
+	public LocaleChangeInterceptor localeInterceptor() {
+		final LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
 
-    @Override
-    public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(localeInterceptor());
-        registry.addInterceptor(patientImportationInterceptor);
-    }
-
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-    	registry.addResourceHandler("swagger-ui.html")
-         .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-         .addResourceLocations("classpath:/META-INF/resources/webjars/");
-
-	    registry.addResourceHandler("/**/*")
-	        .setCachePeriod(0)
-	        .addResourceLocations("classpath:/static/")
-	        .resourceChain(true)
-	        .addResolver(new PathResourceResolver() {
-
-	            @Override
-	            protected Resource getResource(final String resourcePath,
-	                final Resource location) throws IOException {
-	                  final Resource requestedResource = location.createRelative(resourcePath);
-	                  return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-	                : new ClassPathResource("/static/index.html");
-	            }
-
-	        });
-    }
+	@Override
+	public void addInterceptors(final InterceptorRegistry registry) {
+		registry.addInterceptor(localeInterceptor());
+		registry.addInterceptor(patientImportationInterceptor);
+	}
 
 }
