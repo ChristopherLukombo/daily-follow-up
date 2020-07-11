f93657953cc3059837ef87994ebcae77ae4dfc42
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2020-07-11 20:23:38 +0200

    add listner for audit

--- backEnd/src/main/java/fr/almavivahealth/domain/entity/Order.java
+++ backEnd/src/main/java/fr/almavivahealth/domain/entity/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -16,6 +17,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import fr.almavivahealth.domain.enums.OrderStatus;
 import lombok.AllArgsConstructor;
@@ -38,6 +40,7 @@ import lombok.ToString;
 @Data
 @Builder
 @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
